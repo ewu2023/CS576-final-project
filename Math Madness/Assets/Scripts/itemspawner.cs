470d26b69c38429a4129e040f52ba21fc7ca81e9
Christopher Tran <90222475+chrvtran@users.noreply.github.com>
2022-12-19 14:54:14 -0500

    Sounds for powerups
    - keys jingle, power-down, electric

--- Math Madness/Assets/Scripts/itemspawner.cs
+++ Math Madness/Assets/Scripts/itemspawner.cs
@@ -8,10 +8,16 @@ public class ItemSpawner : MonoBehaviour
     public GameObject key;
     public GameObject slow;
     public GameObject speed;
+    public AudioClip getkey_sound;
+    public AudioClip powerdown_sound;
+    public AudioClip electriczap_sound;
 
     // Internal
     private List<GameObject> items;
     private int location_num;
+    private int index;
+    private List<AudioClip> sounds;
+    private AudioSource source;
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +27,24 @@ public class ItemSpawner : MonoBehaviour
         items.Add(slow);
         items.Add(speed);
 
+        source = GetComponent<AudioSource>();
+        sounds = new List<AudioClip>();
+        sounds.Add(getkey_sound);
+        sounds.Add(powerdown_sound);
+        sounds.Add(electriczap_sound);
     }
 
     public void SpawnItem(int location)
     {
         location_num = location;
-        GameObject item = Instantiate(items[Random.Range(0, items.Count)], this.transform) as GameObject;
+        index = Random.Range(0, items.Count);
+        GameObject item = Instantiate(items[index], this.transform) as GameObject;
         item.transform.position = transform.position;
     }
 
     public void freeLocation()
     {
+        source.PlayOneShot(sounds[index]);
         transform.parent.gameObject.GetComponent<ItemManager>().freeLocation(location_num);
     }
 }
