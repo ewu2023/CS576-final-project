b0a87f9b2e70255354b0c8f25515afd6b19a4ce5
Eric Wu <ericwu2019@gmail.com>
2022-12-19 18:54:05 -0500

    Update slow motion powerup

--- Math Madness/Assets/Scripts/Powerups/SlowMotionPowerup.cs
+++ Math Madness/Assets/Scripts/Powerups/SlowMotionPowerup.cs
@@ -2,12 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlowMotionPowerup : Powerup {
-    public override void ApplyEffect() {
-        Time.timeScale = 0.5f;
+[CreateAssetMenu(fileName="New Consumable", menuName="Inventory/SlowPowerup")]
+public class SlowMotionPowerup : Consumable {
+    public float time_scale;
+    public float duration;
+
+    private GameObject player;
+
+    void OnEnable() {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    public override void RemoveEffect() {
+    public override void Consume() {
+        base.Consume();
+        player.GetComponent<PowerupUI>().StartTimer(Effect(player));
+    }
+
+    public IEnumerator Effect(GameObject player) {
+        float cur_time = 0.0f;
+        PowerupUI player_UI = player.GetComponent<PowerupUI>();
+        player_UI.timer_text.gameObject.SetActive(true);
+        player_UI.powerup_name = this.name;
+
+        // APPLY EFFECT HERE
+        Time.timeScale = time_scale;
+
+        while (cur_time <= duration) {
+            // Calculate how much time remains for the powerup
+            float time_remaining = duration - cur_time;
+
+            // Compute minutes and seconds left
+            int minutes_left = (int) time_remaining / 60;
+            int seconds_left = (int) time_remaining % 60;
+
+            // Update player's timer
+            player_UI.UpdateTimer(minutes_left, seconds_left);
+
+            // Wait one tick then increment
+            yield return new WaitForSeconds(1.0f * time_scale);
+            cur_time += 1.0f;
+        }
+
+        // REMOVE EFFECT HERE
         Time.timeScale = 1.0f;
+        player_UI.timer_text.gameObject.SetActive(false);
+        player_UI.powerup_name = "";
     }
 }
