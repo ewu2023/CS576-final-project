fc41c63f66b085ed96d616d25ea7cf44af87d86f
Eric Wu <ericwu2019@gmail.com>
2022-12-20 01:21:23 -0500

    Added audio sources and clips to inventory

--- Math Madness/Assets/Scripts/InventorySystem/InventorySlot.cs
+++ Math Madness/Assets/Scripts/InventorySystem/InventorySlot.cs
@@ -5,9 +5,20 @@ public class InventorySlot : MonoBehaviour {
     // Keep track of current item in the slot
     private Consumable cur_consumable;
 
+    // Keep track of this slot's audio source
+    private AudioSource audio_source;
+
+    // Get sound clips to play
+    public AudioClip speed_clip;
+    public AudioClip slow_clip;
+
     // Get a reference to the icon to display
     public Image icon;
     public Button slotButton;
+
+    void Start() {
+        audio_source = gameObject.GetComponent<AudioSource>();
+    }
 
     public void AddConsumable(Consumable c) {
         cur_consumable = c;
@@ -27,7 +38,15 @@ public class InventorySlot : MonoBehaviour {
 
     public void Use() {
         if (cur_consumable != null) {
+            // Play audio
+            if (cur_consumable.name == "Speed") {
+                audio_source.PlayOneShot(speed_clip);
+            } else if (cur_consumable.name == "Slow") {
+                audio_source.PlayOneShot(slow_clip);
+            }
+
             cur_consumable.Consume();
+
             if (cur_consumable.name != "Key") {
                 PlayerInventory.instance.Remove(cur_consumable);
             }
